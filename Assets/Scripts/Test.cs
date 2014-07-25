27ee30dfe35a3cd4c59d8764644bd8c8f57fcd9b
sunxiaoyu <84136264@qq.com>
2014-07-25 16:02:52 +0800

    init commit

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+	
+	public Rigidbody2D test;		
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print ("Update");
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		print ("FixedUpdate");
+	}
+}
